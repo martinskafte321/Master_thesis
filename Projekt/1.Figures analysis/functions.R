cfed8c8b0f10563f9c1ed715ede365af93005d5f
martinskafte321 <Martinskafte@live.dk>
2023-03-11 14:40:30 +0100

    Plots

--- Projekt/1.Figures analysis/functions.R
+++ Projekt/1.Figures analysis/functions.R
@@ -98,3 +98,17 @@ roll_capm_estimation <- function(data, window, min_obs, period = "day" ) {
   ))
 }
 
+
+winsorize <- function(x, cut) {
+  x <- replace(
+    x,
+    x > quantile(x, 1 - cut, na.rm = T),
+    quantile(x, 1 - cut, na.rm = T)
+  )
+  x <- replace(
+    x,
+    x < quantile(x, cut, na.rm = T),
+    quantile(x, cut, na.rm = T)
+  )
+  return(x)
+}
