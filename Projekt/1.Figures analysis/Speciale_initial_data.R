61b896f74eb98e35fe069cd44b92b892a42351a1
martinskafte321 <Martinskafte@live.dk>
2023-04-23 22:29:21 +0200

    Tables for equal weighted portfolios

--- Projekt/1.Figures analysis/Speciale_initial_data.R
+++ Projekt/1.Figures analysis/Speciale_initial_data.R
@@ -198,3 +198,114 @@ all_data_monthly %>%
          write.csv(file = "data_daily.csv")
 
        
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+# TEst
+
+      stocks = stocks_daily %>% group_by(isin) %>%
+         mutate(date = date(date),
+                Day = weekdays(date),
+                date = case_when(Day == "lørdag" ~ as.character(NA), 
+                                 Day == "søndag" ~ as.character(NA), 
+                                 TRUE ~ as.character(date))) %>%
+         na.omit() %>%
+         group_by(isin) %>%
+         mutate(PX_LAST_lag = lag(PX_LAST,5)) %>%
+         na.omit() %>%
+         mutate(ret = PX_LAST/PX_LAST_lag - 1,
+                date = as.Date(date)) %>%
+        filter(Day  == 'mandag')
+      
+      index = stocks %>% group_by(date) %>%
+        summarise(mean = weighted.mean(ret, MKT_CAP))
+      
+      
+      eu_3factors_daily <- read.csv("C:/Users/Marti/OneDrive - University of Copenhagen/KU/Speciale/Data behandling/Europe_3_Factors_Daily.csv", sep = ",") %>% 
+        transmute(date = as.Date(ymd(X), format = "%Y-%m-%d"),
+                  mkt_excess = as.numeric(Mkt.RF)/100,
+                  SMB = as.numeric(SMB)/100,
+                  HML = as.numeric(HML)/100,
+                  RF = as.numeric(RF)/100) %>%
+        filter(date >= '2018-01-01')   
+      
+      mkt_excess = eu_3factors_daily %>% select(date,mkt_excess)
+      mkt_excess_xts = xts(mkt_excess$mkt_excess, order.by = mkt_excess$date)   
+      mkt_excess_ret= aggregate(mkt_excess_xts, by=week, FUN=dailytoweekly)
+      colnames(mkt_excess_ret)[1]  <- "mkt_excess"
+      
+      
+      SMB = eu_3factors_daily %>% select(date,SMB)
+      SMB_xts = xts(SMB$SMB, order.by = mkt_excess$date)   
+      SMB_ret= aggregate(SMB_xts, by=week, FUN=dailytoweekly)
+      colnames(SMB_ret)[1]  <- "SMB"
+      
+      HML = eu_3factors_daily %>% select(date,HML)
+      HML_xts = xts(HML$HML, order.by = mkt_excess$date)   
+      HML_ret= aggregate(HML_xts, by=week, FUN=dailytoweekly)
+      colnames(HML_ret)[1]  <- "HML"
+      
+      RF = eu_3factors_daily %>% select(date,RF)
+      RF_xts = xts(RF$RF, order.by = mkt_excess$date)   
+      RF_ret = aggregate(RF_xts, by=week, FUN=dailytoweekly)
+      colnames(RF_ret)[1]  <- "RF"
+      
+      
+      factors = data.frame(date=index(mkt_excess_ret), coredata(mkt_excess_ret)) %>% select(mkt_excess)
+      
+      factors = mkt_excess_ret %>%
+        cbind(SMB_ret,HML_ret,RF_ret)
+      
+      index
+      
+      ff3 = data.frame(date=index(factors), coredata(factors))
+      
+      FF3 <- read.csv("FF3_weekly.csv", sep = ";") %>% select(-X) %>% 
+        mutate(
+          date = as.Date(date, format = "%d-%m-%Y"),
+          mkt_excess = as.numeric(mkt_excess),
+          SMB = as.numeric(SMB),
+          HML = as.numeric(HML),
+          RF = as.numeric(RF)) %>%
+      left_join(index, by = "date") %>% mutate(mkt_excess_ret = mean - RF) 
+
+      
+      
+      library(PerformanceAnalytics) # for Return.cumulative function
+         library(quantmod) # loads xts which has apply.monthly function 
+         library(zoo)
+          z = xts(dat$W_RETURN, order.by = as.Date(dat$date))
+      
+       week <- function(x) format(x, '%Y-%W')
+       dailytoweekly <- function(x) (prod(x+1)-1)
+       
+       aggregate(mkt_excess_xts, by=week, FUN=dailytoweekly)
+       
+       # sentiment
+       sentiment = sentiment_daily %>% select(date,isin,sentiment_negative_count,sdg_not_relevant_negative_count) %>% mutate(date = as.Date(date)) %>%
+         group_by(isin) %>%
+       summarize_by_time(
+         .date_var = date,
+         .by = "week",
+         sentiment_negative_count = sum(sentiment_negative_count),
+         sdg_not_relevant_negative_count = sum(sdg_not_relevant_negative_count),
+         .type = c("ceiling"),
+         .week_start = 1
+       )
+
+       data = stocks %>% left_join(sentiment, by = c("date","isin")) %>% transmute(isin,date, 
+                                                                                   norm_negative_sum = sentiment_negative_count - sdg_not_relevant_negative_count,
+                                                                                   ret, MKT_CAP) %>% arrange(isin)
+       
+       data %>%
+         write.csv(file = "data_weekly.csv")
+
+                               
